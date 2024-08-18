01d31251c323f28a27f0df582088f529ffaa611a
atai <ataisamak@gmail.com>
2024-08-18 14:58:55 +0600

    feat: added contacts link

---
+++ src/components/contacts/Contacts.config.ts
@@ -0,0 +1,6 @@
+const contactsConfig = {
+  linkedIn: 'https://www.linkedin.com/in/atai-samakov-b798a42a4/',
+  telegram: 'https://t.me/ataisamak',
+  email: 'ataisamak@gmail.com',
+}
+export default contactsConfig
--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -4,14 +4,22 @@ import { ReactComponent as Email } from '@icons/email.svg'
 import { ReactComponent as LinkedIn } from '@icons/linkedin.svg'
 import { ReactComponent as Telegram } from '@icons/telegram.svg'
 
+import contactsConfig from '@components/contacts/Contacts.config'
+
 import styles from './Contacts.module.scss'
 
 const Contacts = () => {
   return (
     <div className={styles.container}>
-      <LinkedIn className={styles.icon} />
-      <Telegram className={styles.icon} />
-      <Email className={styles.icon} />
+      <a href={contactsConfig.linkedIn} target="_blank" rel="noreferrer" className={styles.link}>
+        <LinkedIn />
+      </a>
+      <a href={contactsConfig.telegram} target="_blank" rel="noreferrer" className={styles.link}>
+        <Telegram />
+      </a>
+      <a href={`mailto:${contactsConfig.email}`} className={styles.link}>
+        <Email />
+      </a>
     </div>
   )
 }

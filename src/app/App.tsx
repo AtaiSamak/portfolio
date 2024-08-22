af80baa3fafec37f6aebe8c2e1e812d51645c20c
atai <ataisamak@gmail.com>
2024-08-22 16:29:02 +0600

    refactor: changed fonts format to woff2 and added preload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 
 import AppDesktop from '@app/desktop/AppDesktop'
 import AppMobile from '@app/mobile/AppMobile'
 
-import Loader from '@components/loader/Loader'
-
 import useIsMobile from '@hooks/useIsMobile'
 
-// import './App.scss'
+import './App.scss'
 
 const App = () => {
   const isMobile = useIsMobile()
 
-  return (
-    <div className={'theme--dark'}>
-      {/*{isMobile ? <AppMobile /> : <AppDesktop />}*/}
-      {/*<Loader />*/}
-    </div>
-  )
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeout(() => {
+        const loader = document.getElementById('loader')
+        loader?.classList.remove('show')
+        setTimeout(() => {
+          loader?.classList.add('hide')
+          setTimeout(() => {
+            loader?.remove()
+            document.getElementById('loader-script')?.remove()
+            document.getElementById('loader-styles')?.remove()
+          }, 200)
+        }, 600)
+      }, 600)
+    }, 200)
+  }, [])
+
+  return <div className={'theme--dark'}>{isMobile ? <AppMobile /> : <AppDesktop />}</div>
 }
 
 export default App
